7bffabb25f137d6ba9d5e7a5fdba546f1eede651
兰草酱 <lanluz@qq.com>
2024-03-10 15:04:15 +0800

    修改 模板插件

--- plugins/test/__init__.py
+++ plugins/test/__init__.py
@@ -1,11 +1,11 @@
 import botpy
 
 __plugin_name__ = "模板插件"
-__plugin_command__ = "测试"
+__plugin_command__ = "模板测试"
 __plugin_author__ = "LanluZ"
 __plugin_version__ = 1.0
 
 
 def __init__(command):
     if command == __plugin_command__:
-        print(2)
+        print("模板测试成功")
