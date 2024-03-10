0e85ef6dcc5d57baaf1d07026890f25d8c2ffbfb
兰草酱 <lanluz@qq.com>
2024-03-10 15:59:06 +0800

    修改 关键词屏蔽

--- plugins/keyword-block/__init__.py
+++ plugins/keyword-block/__init__.py
@@ -1,4 +1,6 @@
-import botpy
+import bot
+
+from botpy.message import Message
 
 __plugin_name__ = "关键词屏蔽"  # 插件名称
 __plugin_iskeyword__ = False  # 是否需要触发关键词
@@ -7,6 +9,6 @@ __plugin_author__ = "LanluZ"  # 插件作者
 __plugin_version__ = 1.0  # 插件版本
 
 
-def __init__(command, message):
+async def __init__(command, client: bot.BotClient, message: Message):
     if not __plugin_iskeyword__ or command == __plugin_command__:
-        print(2)
+        await message.reply(content="1")
