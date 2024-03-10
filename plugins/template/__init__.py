904db2e79e3d55af193e3d34032331dc3b908acb
兰草酱 <lanluz@qq.com>
2024-03-10 15:58:48 +0800

    修改 模板

--- plugins/template/__init__.py
+++ plugins/template/__init__.py
@@ -1,5 +1,7 @@
 import bot
 
+from botpy.message import Message
+
 __plugin_name__ = "模板插件"  # 插件名称
 __plugin_iskeyword__ = True  # 是否需要触发关键词
 __plugin_command__ = "模板插件"  # 触发关键词
@@ -7,6 +9,6 @@ __plugin_author__ = "LanluZ"  # 插件作者
 __plugin_version__ = 1.0  # 插件版本
 
 
-def __init__(client: bot.BotClient, command):
+async def __init__(command, client: bot.BotClient, message: Message):
     if not __plugin_iskeyword__ or command == __plugin_command__:
         print("模板插件")
