96c35af09088c51758972ef9ba5c411798dba5b9
兰草酱 <lanluz@qq.com>
2024-03-10 15:59:33 +0800

    修改 getkit.py

--- getkit.py
+++ getkit.py
@@ -1,6 +1,9 @@
 import os
 
+from botpy.message import Message
 
+
+# 获取插件列表
 def get_plugin_list(plugins_path: str):
     plugin_list = []
 
@@ -10,3 +13,15 @@ def get_plugin_list(plugins_path: str):
         plugin_list.append([module_name, module_path])
 
     return plugin_list
+
+
+# 提取Message中命令部分
+def get_message_command(message: Message):
+    message_content = message.content[message.content.find(" "):]
+    if message_content[0] == "/":  # 是否属于命令调用
+        message_content = message_content.replace("/", "")
+        message_command = message_content.split(" ")[0]  # 提取命令
+        return message_command
+    return None
+
+
