8e9071203e64e7dcd0de21492bba96db8f4befa0
兰草酱 <lanluz@qq.com>
2024-03-10 15:08:11 +0800

    更新 插件模板

--- plugins/test/__init__.py
+++ plugins/test/__init__.py
@@ -1,9 +1,10 @@
 import botpy
 
-__plugin_name__ = "模板插件"
-__plugin_command__ = "模板测试"
-__plugin_author__ = "LanluZ"
-__plugin_version__ = 1.0
+__plugin_name__ = "模板插件"  # 插件名称
+__plugin_iskeyword__ = True  # 是否需要触发关键词
+__plugin_command__ = "模板测试"  # 触发关键词
+__plugin_author__ = "LanluZ"  # 插件作者
+__plugin_version__ = 1.0  # 插件版本
 
 
 def __init__(command):
