8a25217178f6486384f564df9f45bd919522383d
兰草酱 <lanluz@qq.com>
2024-03-10 17:34:25 +0800

    添加 调试插件

---
+++ plugins/debug/__init__.py
@@ -0,0 +1,17 @@
+import bot
+
+from botpy.message import Message
+
+__plugin_name__ = "调试"  # 插件名称
+__plugin_iskeyword__ = True  # 是否需要触发关键词
+__plugin_command__ = ["调试"]  # 触发关键词
+__plugin_author__ = "LanluZ"  # 插件作者
+__plugin_version__ = 1.0  # 插件版本
+
+
+async def __init__(command, client: bot.BotClient, message: Message):
+    if not __plugin_iskeyword__ or command in __plugin_command__:
+        print(message.author)  # 消息来源
+        print(message.content)  # 消息内容
+        print(message.guild_id)  # 频道ID
+        print(message.channel_id)  # 子频道ID
