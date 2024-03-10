0068b613427fcd9f331130b68ab9a4b594e56a65
兰草酱 <lanluz@qq.com>
2024-03-10 15:26:49 +0800

    修改 目录

--- plugins/keyword-block/__init__.py
+++ plugins/keyword-block/__init__.py
@@ -1,11 +1,12 @@
 import botpy
 
-__plugin_name__ = "模板插件"
-__plugin_command__ = "测试"
-__plugin_author__ = "LanluZ"
-__plugin_version__ = 1.0
+__plugin_name__ = "关键词屏蔽"  # 插件名称
+__plugin_iskeyword__ = False  # 是否需要触发关键词
+__plugin_command__ = ""  # 触发关键词
+__plugin_author__ = "LanluZ"  # 插件作者
+__plugin_version__ = 1.0  # 插件版本
 
 
-def __init__(command):
-    if command == __plugin_command__:
+def __init__(command, message):
+    if not __plugin_iskeyword__ or command == __plugin_command__:
         print(2)
