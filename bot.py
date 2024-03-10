47bcb62454fbba72c4e5e96051b4205d18961020
兰草酱 <lanluz@qq.com>
2024-03-10 15:59:26 +0800

    修改 bot.py

--- bot.py
+++ bot.py
@@ -20,10 +20,10 @@ plugins_path = os.path.join(cwd_path, "plugins")
 class BotClient(botpy.Client):
     plugins = dict()
 
-    # @初始化机器人事件
+    # 初始化机器人事件
     async def on_ready(self):  # 准备就绪事件
-        _log.info(f"「{self.robot.name}」初始化")  # 日志
-        _log.info("==========开始载入插件==========")  # 日志
+        _log.info(f"「{self.robot.name}」初始化")
+        _log.info("==========开始载入插件==========")
 
         # 载入插件
         plugin_list = getkit.get_plugin_list(plugins_path)
@@ -41,23 +41,16 @@ class BotClient(botpy.Client):
     async def on_at_message_create(self, message: Message):
         if "sleep" in message.content:
             await asyncio.sleep(10)
-        _log.info(message.author.username)  # 日志-用户名
-
-        # 消息内容提取
-        message_content = message.content[message.content.find(" "):]
-        message_command = ""
-        if message_content[0] == "/":  # 是否属于命令调用
-            message_content = message_content.replace("/", "")
-            message_command = message_content.split(" ")[0]  # 提取命令
+        _log.info(message.author.username)
 
         # 命令匹配
+        message_command = getkit.get_message_command(message)  # 提取会话消息中命令
         for plugin_name in self.plugins:
-            self.plugins[plugin_name].__init__(message_command, message_content)
+            await self.plugins[plugin_name].__init__(message_command, self, message)
 
 
 if __name__ == "__main__":
     # 通过预设置的类型，设置需要监听的事件通道
-    # intents = botpy.Intents.none()
     # intents.public_messages=True
 
     # 通过kwargs，设置需要监听的事件通道
